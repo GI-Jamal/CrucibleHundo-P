0daab3f9fc7502d375b4cb78c0a54d0d50715542
Jamal Gibson <jamal.gibson@rogers.com>
2023-04-11 16:37:55 -0400

    Updated files

--- js/site.js
+++ js/site.js
@@ -1,12 +1,41 @@
-function displayMessage() {
-  let msg = document.getElementById('message').value;
-  //alert(msg);
+//get the start and end numbers from the inputs
+// Entry point AKA Controller function
+function getValue() {
+  
+  let startValue = document.getElementById('startValue').value; // startValue = "0"
+  let endvalue = document.getElementbyId('endValue').value;
 
-  Swal.fire(
+  let startNumber = parseInt(startValue); // startNumber = 0
+  let endNumber = parseInt(endvalue);
+
+  let numbers = generateNumbers(startNumber, endNumber);
+
+  displayNumbers(numbers);
+}
+
+// generate the range of numbers to display
+// Business/logic function
+function generateNumbers(start, end) {
+  
+  let numbers = [];
+  
+  for (let i = start; i <= end; i++)
+  {
+    numbers.push(i);
+  }
+
+  return numbers;
+}
+
+// placing the generate numbers on the page AND bolding the even numbers
+// View function
+function displayNumbers(theNumbers) {
+
+  for (let i = theNumbers[0]; i < theNumbers.length; i++)
+  {
+    if (theNumbers[i] % 2 == 0)
     {
-        backdrop: false,
-        title: 'App Name',
-        text: msg
+      
     }
-  );
+  }
 }
