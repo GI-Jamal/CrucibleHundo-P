dc5d543288fba8a68068ef7ded170e50a98cef57
Jamal Gibson <jamal.gibson@rogers.com>
2023-04-11 18:01:10 -0400

    Updated page

--- js/site.js
+++ js/site.js
@@ -1,12 +1,11 @@
 //get the start and end numbers from the inputs
 // Entry point AKA Controller function
 function getValue() {
-  
-  let startValue = document.getElementById('startValue').value; // startValue = "0"
-  let endvalue = document.getElementbyId('endValue').value;
+  let startValue = document.getElementById("startValue").value; // startValue = "0"
+  let endValue = document.getElementById("endValue").value;
 
   let startNumber = parseInt(startValue); // startNumber = 0
-  let endNumber = parseInt(endvalue);
+  let endNumber = parseInt(endValue);
 
   let numbers = generateNumbers(startNumber, endNumber);
 
@@ -16,11 +15,9 @@ function getValue() {
 // generate the range of numbers to display
 // Business/logic function
 function generateNumbers(start, end) {
-  
   let numbers = [];
-  
-  for (let i = start; i <= end; i++)
-  {
+
+  for (let i = start; i <= end; i++) {
     numbers.push(i);
   }
 
@@ -30,12 +27,22 @@ function generateNumbers(start, end) {
 // placing the generate numbers on the page AND bolding the even numbers
 // View function
 function displayNumbers(theNumbers) {
+  
+  let results = '';
+  
+  for (let i = theNumbers[0]; i < theNumbers.length; i++) {
+    let currentNumber = theNumbers[i];
 
-  for (let i = theNumbers[0]; i < theNumbers.length; i++)
-  {
-    if (theNumbers[i] % 2 == 0)
+    if (currentNumber % 2 == 0)
     {
-      
+      results = results += `<tr><td><strong>${currentNumber}</strong></td></tr>`;
+    }
+    else
+    {
+      results += `<tr><td>${currentNumber}</td></tr>`;
     }
   }
+
+  let tableBody = document.getElementById('results');
+  tableBody.innerHTML = results;
 }
